e948cc88683317cef755fcb18f9815a70e87a5f1
aCampello <a.campello@wellcome.ac.uk>
2020-01-23 16:33:22 +0000

    Add download manager for scibert

--- wellcomeml/ml/bert_vectorizer.py
+++ wellcomeml/ml/bert_vectorizer.py
@@ -3,6 +3,10 @@
 BERT Vectorizer that embeds text using a prertained BERT model
 """
 
+import os
+import tarfile
+
+import boto3
 from transformers import BertModel, BertTokenizer
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
@@ -10,6 +14,14 @@ import torch
 
 from wellcomeml.logger import logger
 
+MODELS_DIR = os.path.expanduser("~/.cache/wellcomeml/models")
+MODEL_DISPATCH = {
+    'scibert_scivocab_uncased': {
+        "bucket": "ai2-s2-research",
+        "path": "scibert/huggingface_pytorch/scibert_scivocab_uncased.tar",
+        "file_name": "scibert_scivocab_uncased.tar"
+        }
+}
 
 class BertVectorizer(BaseEstimator, TransformerMixin):
     def __init__(self, pretrained='bert', sentence_embedding='mean_second_to_last'):
@@ -49,9 +61,36 @@ class BertVectorizer(BaseEstimator, TransformerMixin):
         return np.array([self.bert_embedding(x) for x in X])
 
     def fit(self, *_):
-        model_name = 'bert-base-uncased' if self.pretrained == 'bert' else 'models/scibert_scivocab_uncased'
+        model_name = 'bert-base-uncased' if self.pretrained == 'bert' else 'scibert_scivocab_uncased'
+
+        # If model_name doesn't exist checks cache and change name to
+        # full path
+        if model_name == 'scibert_scivocab_uncased':
+            model_name = _check_cache_and_download(model_name)
+
         logger.info("Using {} embedding".format(model_name))
         self.model = BertModel.from_pretrained(model_name, output_hidden_states=True)
         self.tokenizer = BertTokenizer.from_pretrained(model_name)
         self.model.eval()
         return self
+
+
+def _check_cache_and_download(model_name):
+    """ Checks if model_name is cached and return complete path"""
+    os.makedirs(MODELS_DIR, exist_ok=True)
+
+    model_path = os.path.join(MODELS_DIR, model_name)
+    if not os.path.exists(model_path):
+        logger.info(f"Could not find model {model_name}. Downloading from S3")
+        s3 = boto3.client('s3')
+        tmp_file = os.path.join(MODELS_DIR, MODEL_DISPATCH[model_name]['file_name'])
+
+        s3.download_file(MODEL_DISPATCH[model_name]['bucket'], MODEL_DISPATCH[model_name]['path'], tmp_file)
+
+        tar = tarfile.open(tmp_file)
+        tar.extractall(path=MODELS_DIR)
+        tar.close()
+
+        os.remove(tmp_file)
+
+    return model_path
