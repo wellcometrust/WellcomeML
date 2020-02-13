272f45002419ea2d560f0fd76d01185701f32599
Matthew Upson <matthew.a.upson@gmail.com>
2020-02-13 15:18:31 -0300

    new: Add numbered_reference_annotator

---
+++ wellcomeml/__main__.py
@@ -0,0 +1,31 @@
+# coding: utf8
+
+"""
+Modified from https://github.com/explosion/spaCy/blob/master/spacy/__main__.py
+
+Allows CLI functions defined by plac (or argparse) to be called using the
+following syntax:
+
+`python -m wellcomeml <command>`
+"""
+
+if __name__ == "__main__":
+    import plac
+    import sys
+    from wasabi import msg
+    from .prodigy.numbered_reference_annotator import annotate_numbered_references
+
+    commands = {
+        "annotate_numbered_refs": annotate_numbered_references,
+    }
+
+    if len(sys.argv) == 1:
+        msg.info("Available commands", ", ".join(commands), exits=1)
+    command = sys.argv.pop(1)
+    sys.argv[0] = "wellcomeml %s" % command
+
+    if command in commands:
+        plac.call(commands[command], sys.argv[1:])
+    else:
+        available = "Available: {}".format(", ".join(commands))
+        msg.fail("Unknown command: {}".format(command), available, exits=1)
