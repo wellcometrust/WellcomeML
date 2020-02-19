34ca3461e8ae12732d49ede4cf39d9123df91133
Nick Sorros <nsorros@gmail.com>
2020-02-19 10:43:21 +0200

    Update version

--- wellcomeml/__version__.py
+++ wellcomeml/__version__.py
@@ -1,5 +1,5 @@
 __name__='wellcomeml'
-__version__='2020.2.1'
+__version__='2020.2.2'
 __description__='Utilities for managing nlp models and for processing text-related data at Wellcome Data Labs'
 __url__='https://github.com/wellcometrust/datalabs/tree/master/code/wellcomeml/'
 __author__='Wellcome Trust DataLabs Team'
