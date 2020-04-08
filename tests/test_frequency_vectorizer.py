7379282f6aad54138fc141fc46469b39e56b7373
aCampello <a.campello@wellcome.ac.uk>
2020-04-08 12:55:12 +0100

    Add tests for saving and loading

--- tests/test_frequency_vectorizer.py
+++ tests/test_frequency_vectorizer.py
@@ -9,3 +9,20 @@ def test_tf_idf_dispatch():
     X_embed = text_vectorizer.fit_transform(X)
 
     assert (X_embed.shape == (1, 3))
+
+
+def test_save_and_load(tmpdir):
+    tmpfile = tmpdir.join('test.npy')
+
+    X = ["This is a sentence"*100]
+
+
+    vec = WellcomeTfidf()
+
+    X_embed = vec.fit_transform(X)
+
+    vec.save_transformed(str(tmpfile), X_embed)
+
+    X_loaded = vec.load_transformed(str(tmpfile))
+
+    assert (X_loaded != X_embed).sum() == 0
