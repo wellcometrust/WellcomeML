3d1c2b1b8cb5ffb380a3367ad41feeb947c2cf89
Nick Sorros <nsorros@gmail.com>
2020-04-17 14:34:33 +0300

    Add test for keras vectorizer

---
+++ tests/test_keras_vectorizer.py
@@ -0,0 +1,29 @@
+from wellcomeml.ml import KerasVectorizer
+
+def test_vanilla():
+    X = ["One", "Two", "Three"]
+
+    keras_vectorizer = KerasVectorizer()
+    X_vec = keras_vectorizer.fit_transform(X)
+
+    assert X_vec.shape[0] == 3
+    assert X_vec.shape[1] == 100 # default sequence length
+    assert X_vec.max() == 4 # 4 tokens including OOV
+
+def test_sequence_length():
+    X = ["One", "Two", "Three"]
+
+    sequence_length = 5
+    keras_vectorizer = KerasVectorizer(sequence_length=sequence_length)
+    X_vec = keras_vectorizer.fit_transform(X)
+
+    assert X_vec.shape[1] == sequence_length
+
+def test_vocab_size():
+    X = ["One", "Two", "Three"]
+
+    vocab_size = 1
+    keras_vectorizer = KerasVectorizer(vocab_size=vocab_size)
+    X_vec = keras_vectorizer.fit_transform(X)
+
+    assert X_vec.max() == vocab_size
