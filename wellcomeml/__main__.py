0413a8dadb7fce5f6c2b344954ee7140068c41d5
Nick Sorros <nsorros@gmail.com>
2020-05-15 12:03:36 +0300

    Fix running virtualenv python from main

--- wellcomeml/__main__.py
+++ wellcomeml/__main__.py
@@ -1,5 +1,13 @@
 import subprocess
 import sys
+import os
+
+
+VENV_PYTHON = os.path.join(
+    os.path.dirname(__file__),
+    '..', 'build', 'virtualenv',
+    'bin', 'python'
+)
 
 if __name__ == '__main__':
     command = sys.argv.pop(1)
@@ -9,8 +17,10 @@ if __name__ == '__main__':
 
     download_target = sys.argv.pop(1)
     if download_target == "models":
-        subprocess.run(['python', '-m', 'spacy', 'download', 'en_core_web_sm'])
+        subprocess.run([
+            VENV_PYTHON, '-m', 'spacy', 'download', 'en_core_web_sm'])
     elif download_target == "deeplearning-models":
-        subprocess.run(['python', '-m', 'spacy', 'download', 'en_trf_bertbaseuncased_lg'])
+        subprocess.run([
+            VENV_PYTHON, '-m', 'spacy', 'download', 'en_trf_bertbaseuncased_lg'])
     else:
         print(f"{download_target} is not one of models,deeplearning-models")
