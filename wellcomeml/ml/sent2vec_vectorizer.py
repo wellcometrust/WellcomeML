24d895884e845c3ae959f19a673a4ae0b5004c0c
Ubuntu <ubuntu@ip-10-60-51-215.eu-west-1.compute.internal>
2020-06-03 11:54:19 +0000

    Add sent2vec transform

---
+++ wellcomeml/ml/sent2vec_vectorizer.py
@@ -0,0 +1,25 @@
+"""
+Vectorizer that exposes sklearn interface to sent2vec
+paper and codebase. https://github.com/epfml/sent2vec
+"""
+from sklearn.base import TransformerMixin, BaseEstimator
+import sent2vec
+
+from wellcomeml.utils import check_cache_and_download
+
+class Sent2VecVectorizer(BaseEstimator, TransformerMixin):
+    def __init__(self, pretrained=None):
+        self.pretrained=pretrained
+
+    def fit(self):
+        if self.pretrained:
+            model_path = check_cache_and_download(self.pretrained)
+            self.model = sent2vec.Sent2vecModel()
+            self.model.load_model(model_path)
+        else:
+            # Custom training not yet implemented
+            pass
+        return self
+
+    def transform(self, X):
+        return self.model.embed_sentences(X)
