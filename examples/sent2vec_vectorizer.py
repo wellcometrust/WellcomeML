e3b0560db6bcd4b695ba2b281fcf62ee8a4a8a7c
Ubuntu <ubuntu@ip-10-60-51-215.eu-west-1.compute.internal>
2020-06-04 14:21:47 +0000

    Fix sent2vec example

--- examples/sent2vec_vectorizer.py
+++ examples/sent2vec_vectorizer.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 
 
-from wellcomeml.ml.sent2vec_vectorizer import Sent2VecVectorizer
+from wellcomeml.ml import Sent2VecVectorizer
 
 X = [
     "Malaria is a disease spread by mosquitos",
@@ -11,14 +11,14 @@ X = [
     "Trump is the president of USA"
 ]
 
-sent2vec = Sent2VecVectorizer()
+sent2vec = Sent2VecVectorizer("sent2vec_wiki_unigrams")
 X_transformed = sent2vec.fit_transform(X)
 print(cosine_similarity(X_transformed))
 
 y = [1,1,0]
 
 model = Pipeline([
-    ('sent2vec', Sent2VecVectorizer()),
+    ('sent2vec', Sent2VecVectorizer("sent2vec_wiki_unigrams")),
     ('sgd', SGDClassifier())
 ])
 model.fit(X, y)
