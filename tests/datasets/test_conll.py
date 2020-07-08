fed88dae7de0af028be94f5e702bdbbe71d6e8c4
aCampello <a.campello@wellcome.ac.uk>
2020-07-08 13:20:03 +0100

    Fix tests/datasets

--- tests/datasets/test_conll.py
+++ tests/datasets/test_conll.py
@@ -1,11 +1,11 @@
-import pytest
-
 from wellcomeml.datasets.conll import _load_data_spacy
+
 
 def test_length():
     X, Y = _load_data_spacy('tests/test_data/test_conll', inc_outside=True)
 
     assert len(X) == len(Y) and len(X) == 4
+
 
 def test_entity():
     X, Y = _load_data_spacy('tests/test_data/test_conll', inc_outside=False)
@@ -15,9 +15,10 @@ def test_entity():
 
     assert X[0][start:end] == 'LEICESTERSHIRE'
 
+
 def test_no_outside_entities():
     X, Y = _load_data_spacy('tests/test_data/test_conll', inc_outside=False)
 
-    outside_entities = [entity for entities in Y for entity in entities if entity['label']=='O']
+    outside_entities = [entity for entities in Y for entity in entities if entity['label'] == 'O']
 
     assert len(outside_entities) == 0
