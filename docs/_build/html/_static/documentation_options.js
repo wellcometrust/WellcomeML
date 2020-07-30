0666763c640157f84163981117899b0fce595c72
aCampello <a.campello@wellcome.ac.uk>
2020-07-30 11:38:16 +0100

    Update docs

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2020.5.1',
+    VERSION: 'v2020.7.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
