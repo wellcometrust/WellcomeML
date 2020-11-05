d05af1d7344114879c37fcc95dd97a2bf47b06b4
Matthew Upson <matthew.a.upson@gmail.com>
2020-11-05 13:15:59 -0300

    chg: Update __init__.py

--- wellcomeml/metrics/__init__.py
+++ wellcomeml/metrics/__init__.py
@@ -1,3 +1,4 @@
+from .f1 import GlobalF1, f1_loss, f1_metric
 from .ner_classification_report import ner_classification_report
 
-__all__ = ['ner_classification_report']
+__all__ = ["ner_classification_report", "f1_metric", "f1_loss", "GlobalF1"]
