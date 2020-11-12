331a969ee6c0d2b90ff3e3827493b17b882630d7
Matthew Upson <matthew.a.upson@gmail.com>
2020-11-12 10:52:09 +0000

    chg: Reduce replication

--- wellcomeml/metrics/f1.py
+++ wellcomeml/metrics/f1.py
@@ -37,14 +37,4 @@ def f1_loss(y_true, y_pred):
     >>>     metrics=["accuracy"]
     >>> )
     """
-    y_true = K.cast(y_true, "float")
-    y_pred = K.cast(y_pred, "float")
-    tp = K.sum(K.cast(y_true * y_pred, "float"), axis=0)
-    fp = K.sum(K.cast((1 - y_true) * y_pred, "float"), axis=0)
-    fn = K.sum(K.cast(y_true * (1 - y_pred), "float"), axis=0)
-    p = tp / (tp + fp + K.epsilon())
-    r = tp / (tp + fn + K.epsilon())
-    f1 = 2 * p * r / (p + r + K.epsilon())
-    f1 = tf.where(tf.math.is_nan(f1), tf.zeros_like(f1), f1)
-
-    return 1 - K.mean(f1)
+    return 1 - f1_metric(y_true, y_pred)
