1790881e9229176b5503bbae85ec4126210ffcb5
Nick Sorros <nsorros@gmail.com>
2020-11-18 17:22:18 +0200

    Update version to 2020.11.0

--- wellcomeml/__version__.py
+++ wellcomeml/__version__.py
@@ -1,5 +1,5 @@
 __name__ = "wellcomeml"
-__version__ = "2020.9.0"
+__version__ = "2020.11.0"
 __description__ = """Utilities for managing nlp models and for
                      processing text-related data at Wellcome Data Labs"""
 __url__ = "https://github.com/wellcometrust/wellcomeml/tree/master"
