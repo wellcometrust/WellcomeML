6de51622792fa303bca5990199b5bef221d22cad
Nick Sorros <nsorros@gmail.com>
2020-11-26 18:54:01 +0200

    Fix tests

--- wellcomeml/ml/bilstm.py
+++ wellcomeml/ml/bilstm.py
@@ -218,11 +218,11 @@ class BiLSTMClassifier(BaseEstimator, ClassifierMixin):
             Y_pred = vstack(Y_pred)
             return Y_pred
         else:
-            return self.model.predict(X, self.batch_size).numpy() > self.threshold
+            return self.model.predict(X, self.batch_size) > self.threshold
 
     def predict_proba(self, X):
         # sparse_y not relevant as probs are dense
-        return self.model.predict(X, self.batch_size).numpy()
+        return self.model.predict(X, self.batch_size)
 
     def score(self, X, Y):
         if isinstance(X, list):
