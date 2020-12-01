e5c98a365e0bfd746accc063a51a7fd1f81d80a8
Nick Sorros <nsorros@gmail.com>
2020-12-01 08:28:31 +0000

    Update setup to install latest spacy with transformers

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
         'boto3',
         'scikit-learn',
         'scipy==1.4.1',
-        'spacy==2.3',
+        'spacy-nightly[transformers]',
         'umap-learn',
         'nervaluate',
         'twine',
@@ -61,9 +61,9 @@ setuptools.setup(
         'deep-learning': [
             'tensorflow',
             'torch',
-            'transformers<2.9.0',
-            'spacy-transformers==0.6.1',
-            'dataclasses==0.6'  # spacy transformers needs this pinned
+            'transformers==3.0.0',
+#            'spacy-transformers==0.6.1',
+#            'dataclasses==0.6'  # spacy transformers needs this pinned
         ]
     },
     tests_require=[
