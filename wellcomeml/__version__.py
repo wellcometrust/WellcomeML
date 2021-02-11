b47f6c260cc6a859178d02eb218bd56ac0929ddd
Nick Sorros <nsorros@gmail.com>
2021-02-11 12:09:29 +0200

    Update version to 2021.02.1

--- wellcomeml/__version__.py
+++ wellcomeml/__version__.py
@@ -1,5 +1,5 @@
 __name__ = "wellcomeml"
-__version__ = "2021.2.0"
+__version__ = "2021.2.1"
 __description__ = """Utilities for managing nlp models and for
                      processing text-related data at Wellcome Data Labs"""
 __url__ = "https://github.com/wellcometrust/wellcomeml/tree/master"
