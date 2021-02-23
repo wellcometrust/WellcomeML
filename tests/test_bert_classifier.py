ca5d1c38f28a50aacdefa5b5b47647389194e826
Nick Sorros <nsorros@gmail.com>
2021-02-23 12:07:47 +0200

    Add test to avoid the problem in the future

--- tests/test_bert_classifier.py
+++ tests/test_bert_classifier.py
@@ -1,4 +1,6 @@
 # encoding: utf-8
+import tempfile
+
 import numpy as np
 
 from wellcomeml.ml.bert_classifier import BertClassifier
@@ -82,6 +84,41 @@ def test_scibert():
     model.fit(X, Y)
     Y_pred = model.predict(X)
     Y_prob_pred = model.predict_proba(X)
+    assert Y_pred.sum() != 0
+    assert Y_pred.sum() != Y.size
+    assert Y_prob_pred.max() <= 1
+    assert Y_prob_pred.min() >= 0
+    assert Y_pred.shape == Y.shape
+    assert Y_prob_pred.shape == Y.shape
+    assert model.losses[0] > model.losses[-1]
+
+
+def test_save_load():
+    X = [
+        "One and two",
+        "One only",
+        "Three and four, nothing else",
+        "Two nothing else",
+        "Two and three"
+    ]
+    Y = np.array([
+        [1, 1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 1, 1],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0]
+    ])
+
+    model = BertClassifier()
+    model.fit(X, Y)
+
+    with tempfile.TemporaryDirectory() as tmp_path:
+        model.save(tmp_path)
+        loaded_model = BertClassifier()
+        loaded_model.load(tmp_path)
+
+    Y_pred = loaded_model.predict(X)
+    Y_prob_pred = loaded_model.predict_proba(X)
     assert Y_pred.sum() != 0
     assert Y_pred.sum() != Y.size
     assert Y_prob_pred.max() <= 1
