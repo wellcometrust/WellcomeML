3de0601d078604fd2f3166b55142e716a9a865ae
Ubuntu <ubuntu@ip-10-60-51-91.eu-west-1.compute.internal>
2021-03-02 17:06:01 +0000

    Fix predict

--- wellcomeml/ml/bert_semantic_equivalence.py
+++ wellcomeml/ml/bert_semantic_equivalence.py
@@ -240,10 +240,12 @@ class SemanticEquivalenceClassifier(BaseEstimator, TransformerMixin):
         if isinstance(self.strategy, tf.distribute.MirroredStrategy):
             predictions = []
             for batch in self._prep_dataset_generator(X):
-                predictions += [self.model.predict(batch).logits]
+                predictions += [self.model(batch).logits]
+            
+            predictions = tf.concat(predictions, axis=0)
         else:
             predictions = self.model.predict(self._prep_dataset_generator(X)).logits
-
+        
         return tf.nn.softmax(predictions).numpy()
 
     def predict(self, X):
