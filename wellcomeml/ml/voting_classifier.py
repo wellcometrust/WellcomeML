9f4b128991fb3825f5d4bb611714952ee6ede15c
Elizabeth Gallagher <lizgzil@hotmail.com>
2021-03-12 11:39:08 +0000

    Add ability to specify number of models that need to agree in voting classifier

--- wellcomeml/ml/voting_classifier.py
+++ wellcomeml/ml/voting_classifier.py
@@ -11,14 +11,15 @@ import numpy as np
 
 
 class WellcomeVotingClassifier(VotingClassifier):
-    def __init__(self, multilabel=False, *args, **kwargs):
+    def __init__(self, multilabel=False, num_agree=None, *args, **kwargs):
         super(WellcomeVotingClassifier, self).__init__(*args, **kwargs)
         self.pretrained = self._is_pretrained()
         self.multilabel = multilabel
+        self.num_agree = num_agree
 
     def _is_pretrained(self):
         try:
-            check_is_fitted(self, 'estimators')
+            check_is_fitted(self, "estimators")
         except NotFittedError:
             return False
         return True
@@ -37,14 +38,19 @@ class WellcomeVotingClassifier(VotingClassifier):
 
     def predict(self, X):
         if self.pretrained:
-            check_is_fitted(self, 'estimators')
+            check_is_fitted(self, "estimators")
 
             estimators = self._get_estimators()
             has_vectorizers = self._check_vectorizer(estimators)
-            
+
             if self.voting == "soft":
                 if has_vectorizers:
-                    Y_probs = np.array([est.predict_proba(vect.transform(X)) for est, vect in estimators])
+                    Y_probs = np.array(
+                        [
+                            est.predict_proba(vect.transform(X))
+                            for est, vect in estimators
+                        ]
+                    )
                 else:
                     Y_probs = np.array([est.predict_proba(X) for est in estimators])
                 Y_prob = np.mean(Y_probs, axis=0)
@@ -53,19 +59,30 @@ class WellcomeVotingClassifier(VotingClassifier):
                 else:
                     return np.argmax(Y_prob, axis=1)
             else:  # hard voting
+
                 if has_vectorizers:
-                    Y_preds = [est.predict(vect.transform(X)) for est, vect in estimators]
+                    Y_preds = [
+                        est.predict(vect.transform(X)) for est, vect in estimators
+                    ]
                 else:
                     Y_preds = [est.predict(X) for est in estimators]
+                Y_preds = np.array(Y_preds)
                 if self.multilabel:
-                    Y_preds = np.array(Y_preds)
-                    axis = 0
+                    return np.apply_along_axis(
+                        lambda x: np.argmax(np.bincount(x)), axis=0, arr=Y_preds
+                    )
                 else:
-                    Y_preds = np.column_stack(Y_preds)
-                    axis = 1
-                return np.apply_along_axis(
-                    lambda x: np.argmax(np.bincount(x)),
-                    axis=axis,
-                    arr=Y_preds)
+                    # If num_agree isn't set then use majority vote
+                    # TO DO: a message when if num_agree is specified, but
+                    # voting=soft and/or multilabel=True then num_agree isn't used
+                    # 'num_agree specified but not used - only used in binary hard voting cases'
+                    if not self.num_agree:
+                        # So if 4 estimators, >= 3 need to agree
+                        self.num_agree = np.ceil((len(estimators) + 1) / 2)
+
+                    return np.apply_along_axis(
+                        lambda x: int(np.sum(x) >= self.num_agree), axis=0, arr=Y_preds
+                    )
+
         else:
             return super(WellcomeVotingClassifier, self).predict(X)
