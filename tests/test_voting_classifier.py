afbdcc9c4aa2ff3ee20144f9429259d83479c229
Elizabeth Gallagher <lizgzil@hotmail.com>
2021-03-19 16:40:26 +0000

    Add tests for nuances of voting classifier

--- tests/test_voting_classifier.py
+++ tests/test_voting_classifier.py
@@ -153,37 +153,76 @@ def test_hard_voting():
     Y = voting_classifier.predict(X)
     assert np.array_equal(Y, Y_expected)
 
-
-def test_inc_vectorizer():
-
+def test_multilabel_hard():
+    # Since no num_agree, majority (>=2/3) agreement needed
     Y1_prob = np.array([
-        [0.9, 0.1],
-        [0.2, 0.8],
-        [0.3, 0.7]
+        [0.9, 0.9],
+        [0.1, 0.9],
+        [0.1, 0.9]
     ])
     Y2_prob = np.array([
-        [0.7, 0.3],
+        [0.9, 0.9],
+        [0.9, 0.9],
+        [0.1, 0.1]
+    ])
+    Y3_prob = np.array([
+        [0.9, 0.9],
         [0.9, 0.1],
-        [0.4, 0.6]
+        [0.1, 0.1]
     ])
     Y_expected = np.array([
-        0,
-        0,
-        1
+        [1, 1],
+        [1, 1],
+        [0, 0]
     ])
-
-    pipe1 = Pipeline([('vect', MockVectorizer(size=3)), ('est', MockEstimator(Y1_prob))])
-    pipe2 = Pipeline([('vect', MockVectorizer(size=5)), ('est', MockEstimator(Y2_prob))])
+    est1 = MockEstimator(Y1_prob, multilabel=True)
+    est2 = MockEstimator(Y2_prob, multilabel=True)
+    est3 = MockEstimator(Y3_prob, multilabel=True)
 
     voting_classifier = WellcomeVotingClassifier(
-        estimators=[pipe1, pipe2], voting="soft"
+        estimators=[est1, est2, est3], voting="hard", multilabel=True
     )
     X = ["mock", "data", "not used"]
     Y = voting_classifier.predict(X)
     assert np.array_equal(Y, Y_expected)
 
+def test_multilabel_hard_num_agree():
+    # >=3/3 agreement needed
+    Y1_prob = np.array([
+        [0.9, 0.9],
+        [0.1, 0.9],
+        [0.1, 0.9]
+    ])
+    Y2_prob = np.array([
+        [0.9, 0.9],
+        [0.9, 0.9],
+        [0.1, 0.1]
+    ])
+    Y3_prob = np.array([
+        [0.9, 0.9],
+        [0.9, 0.1],
+        [0.1, 0.1]
+    ])
+    Y_expected = np.array([
+        [1, 1],
+        [0, 0],
+        [0, 0]
+    ])
+    est1 = MockEstimator(Y1_prob, multilabel=True)
+    est2 = MockEstimator(Y2_prob, multilabel=True)
+    est3 = MockEstimator(Y3_prob, multilabel=True)
 
-def test_binary_hard_majority():
+    voting_classifier = WellcomeVotingClassifier(
+        estimators=[est1, est2, est3],
+        voting="hard",
+        multilabel=True,
+        num_agree=3
+    )
+    X = ["mock", "data", "not used"]
+    Y = voting_classifier.predict(X)
+    assert np.array_equal(Y, Y_expected)
+
+def test_binary_hard():
 
     Y1_prob = np.array([
         [0.9, 0.1],
@@ -247,7 +286,7 @@ def test_binary_hard_num_agree():
     ])
     Y_expected = np.array([
         0,
-        1,
+        0,
         1
     ])
     est1 = MockEstimator(Y1_prob)
@@ -257,6 +296,111 @@ def test_binary_hard_num_agree():
 
     voting_classifier = WellcomeVotingClassifier(
         estimators=[est1, est2, est3, est4], voting="hard", num_agree=2
+    )
+    X = ["mock", "data", "not used"]
+    Y = voting_classifier.predict(X)
+    assert np.array_equal(Y, Y_expected)
+
+def test_multiclass_hard():
+
+    Y1_prob = np.array([
+        [0.6, 0.1, 0.1, 0.2],
+        [0.2, 0.3, 0.1, 0.4],
+        [0.3, 0.1, 0.4, 0.2]
+    ])
+    Y2_prob = np.array([
+        [0.5, 0.3, 0.2, 0.0],
+        [0.1, 0.6, 0.1, 0.2],
+        [0.1, 0.3, 0.1, 0.5]
+    ])
+    Y3_prob = np.array([
+        [0.5, 0.3, 0.2, 0.0],
+        [0.2, 0.3, 0.1, 0.4],
+        [0.1, 0.5, 0.1, 0.3]
+    ])
+    # Using highest values:
+    # 1st data point: 0: 3 votes -> clear winner is 0
+    # 2nd data point: 1: 1 vote, 3: 2 votes -> clear winner (in majority voting) is 3
+    # 3rd data point: 2: 1 vote, 3: 1 vote, 1: 1 vote -> no winner, pick first in numerical order, so 1
+    Y_expected = np.array([
+        0,
+        3,
+        1
+    ])
+    est1 = MockEstimator(Y1_prob)
+    est2 = MockEstimator(Y2_prob)
+    est3 = MockEstimator(Y3_prob)
+
+    voting_classifier = WellcomeVotingClassifier(
+        estimators=[est1, est2, est3], voting="hard"
+    )
+    X = ["mock", "data", "not used"]
+    Y = voting_classifier.predict(X)
+
+    assert np.array_equal(Y, Y_expected)
+
+def test_multiclass_hard_num_agree():
+
+    Y1_prob = np.array([
+        [0.6, 0.1, 0.1, 0.2],
+        [0.2, 0.3, 0.1, 0.4],
+        [0.3, 0.1, 0.4, 0.2]
+    ])
+    Y2_prob = np.array([
+        [0.5, 0.3, 0.2, 0.0],
+        [0.1, 0.6, 0.1, 0.2],
+        [0.1, 0.3, 0.1, 0.5]
+    ])
+    Y3_prob = np.array([
+        [0.5, 0.3, 0.2, 0.0],
+        [0.2, 0.3, 0.1, 0.4],
+        [0.1, 0.5, 0.1, 0.3]
+    ])
+    # Using highest values:
+    # 1st data point: 0: 3 votes -> clear winner is 0
+    # 2nd data point: 1: 1 vote, 3: 2 votes -> no winner, pick first in numerical order, so 1
+    # 3rd data point: 2: 1 vote, 3: 1 vote, 1: 1 vote -> no winner, pick first in numerical order, so 1
+    Y_expected = np.array([
+        0,
+        1,
+        1
+    ])
+    est1 = MockEstimator(Y1_prob)
+    est2 = MockEstimator(Y2_prob)
+    est3 = MockEstimator(Y3_prob)
+
+    voting_classifier = WellcomeVotingClassifier(
+        estimators=[est1, est2, est3],
+        voting="hard",
+        num_agree=3
+    )
+    X = ["mock", "data", "not used"]
+    Y = voting_classifier.predict(X)
+    assert np.array_equal(Y, Y_expected)
+
+def test_inc_vectorizer():
+
+    Y1_prob = np.array([
+        [0.9, 0.1],
+        [0.2, 0.8],
+        [0.3, 0.7]
+    ])
+    Y2_prob = np.array([
+        [0.7, 0.3],
+        [0.9, 0.1],
+        [0.4, 0.6]
+    ])
+    Y_expected = np.array([
+        0,
+        0,
+        1
+    ])
+
+    pipe1 = Pipeline([('vect', MockVectorizer(size=3)), ('est', MockEstimator(Y1_prob))])
+    pipe2 = Pipeline([('vect', MockVectorizer(size=5)), ('est', MockEstimator(Y2_prob))])
+
+    voting_classifier = WellcomeVotingClassifier(
+        estimators=[pipe1, pipe2], voting="soft"
     )
     X = ["mock", "data", "not used"]
     Y = voting_classifier.predict(X)
