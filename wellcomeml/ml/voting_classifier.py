16f380ae83dd59c4b0979b1a5c2fe54849c76941
aCampello <a.campello@wellcome.ac.uk>
2021-04-12 12:01:23 +0100

    Flake8 fixes

--- wellcomeml/ml/voting_classifier.py
+++ wellcomeml/ml/voting_classifier.py
@@ -6,30 +6,30 @@ Some of the code is taken from https://gist.github.com/tomquisel/a421235422fdf6b
 
 voting = "soft":
     multilabel = True:
-        The predicted classes will be any classes where the mean probability from all the 
-        estimators is > 0.5.
+       The predicted classes will be any classes where the mean probability from all the
+    estimators is > 0.5.
     multilabel = False:
-        The predicted class will be class with the largest mean probability from all the 
+        The predicted class will be class with the largest mean probability from all the
         estimators.
 
 voting = "hard":
-    The predicted class(es) will be any class(es) where the majority (or >= num_agree) of 
+    The predicted class(es) will be any class(es) where the majority (or >= num_agree) of
     estimators agree. Majority in even cases is 2/2 or >=3/4 (i.e. not 2/4).
 
-    If there are multiple classes with the majority (a tie) then the first one in 
+    If there are multiple classes with the majority (a tie) then the first one in
     numerical order will be chosen.
 
     In the multiclass case, if there are multiple classes with >= num_agree, then the
     predicted class will be the first class in a numerically sorted list of the tying classes.
     e.g.
-        If there is 1 vote for class 0, 2 votes for class 1 and 2 votes for class 2, 
+        If there is 1 vote for class 0, 2 votes for class 1 and 2 votes for class 2,
         then class 1 will be predicted.
 
     In the binary case, if there is a tie then class 0 is predicted.
-    e.g. 
+    e.g.
         If there are 2 votes for class 0 and 2 votes for class 1, then class 0
          will be predicted.
-    
+
 """
 
 from sklearn.utils.validation import check_is_fitted
@@ -88,11 +88,14 @@ class WellcomeVotingClassifier(VotingClassifier):
                     return np.array(Y_preds.sum(axis=0) >= self.num_agree, dtype='int32')
                 else:
                     votes = np.apply_along_axis(lambda x: max(np.bincount(x)), axis=0, arr=Y_preds)
-                    max_class = np.apply_along_axis(lambda x: np.argmax(np.bincount(x)), axis=0, arr=Y_preds)
+                    max_class = np.apply_along_axis(
+                        lambda x: np.argmax(np.bincount(x)), axis=0, arr=Y_preds
+                    )
                     # If no maximum over the threshold, then pick the first from an ordered list
                     # of the other options, e.g. if 5,2,3 were voted on pick 2 (not 0)
                     options = np.sort(np.transpose(Y_preds))
-                    return [m if v >= self.num_agree else options[i][0] for i, (m,v) in enumerate(zip(max_class, votes))]
+                    return [m if v >= self.num_agree else options[i][0]
+                            for i, (m, v) in enumerate(zip(max_class, votes))]
 
         else:
             return super(WellcomeVotingClassifier, self).predict(X)
