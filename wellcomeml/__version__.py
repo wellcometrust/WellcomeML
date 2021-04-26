92d541181be1b90ae9f0582c0b3ea9df030d6744
aCampello <a.campello@wellcome.ac.uk>
2021-04-26 16:04:28 +0100

    Update version

--- wellcomeml/__version__.py
+++ wellcomeml/__version__.py
@@ -1,5 +1,5 @@
 __name__ = "wellcomeml"
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 __description__ = """Utilities for managing nlp models and for
                      processing text-related data at Wellcome Data Labs"""
 __url__ = "https://github.com/wellcometrust/wellcomeml/tree/main"
