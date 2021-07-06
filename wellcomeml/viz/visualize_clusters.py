ff277cdc7a23b582b055f358a1862439c54cad28
Federica Trevisan <trevisan.federica3@gmail.com>
2021-07-06 17:09:08 +0200

    Update visualize_clusters.py
    fixed code with flake8

--- wellcomeml/viz/visualize_clusters.py
+++ wellcomeml/viz/visualize_clusters.py
@@ -1,27 +1,29 @@
 from bokeh.plotting import figure, show
+from wellcomeml.viz.palettes import Wellcome33
 from bokeh.io import output_notebook
 output_notebook()
-from wellcomeml.viz.palettes import Wellcome33
 
-def visualize_clusters(reduced_points: list): 
+
+def visualize_clusters(reduced_points: list):
 
     """
     Visualises clusters and shows basic information
     Args:
-        reduced_points : list
+        reduced_points: list
         List of list of reduced points. Available at cluster.reduced_points
 
     Returns:
         p
 
     """
+
     blue_wellcome = str(Wellcome33[0])
-    TOOLS="hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,"
-    TOOLTIPS = [("index", "$index"), ("(x,y)", "($x, $y)"),]
+    TOOLS = "hover, crosshair, pan, wheel_zoom, zoom_in, zoom_out, box_zoom, undo, redo, reset, tap, save, box_select, poly_select, lasso_select, "
+    TOOLTIPS = [("index", "$index"), ("(x,y)", "($x, $y)"), ]
 
     p = figure(tools=TOOLS, tooltips=TOOLTIPS)
 
-    p.scatter(reduced_points[:,0], reduced_points[:,1], radius=0.30, fill_color=blue_wellcome, line_color=None, alpha=0.5)
-    
+    p.scatter(reduced_points[:, 0], reduced_points[:, 1], radius=0.30,
+              fill_color=blue_wellcome, line_color=None, alpha=0.5)
+
     show(p)
-    
