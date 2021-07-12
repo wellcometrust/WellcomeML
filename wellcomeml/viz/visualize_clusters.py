e64c05aab4fc4d5e56c01126b4358c6df6f30821
Federica Trevisan <trevisan.federica3@gmail.com>
2021-07-12 15:26:51 +0200

    lowered variable names

--- wellcomeml/viz/visualize_clusters.py
+++ wellcomeml/viz/visualize_clusters.py
@@ -3,8 +3,9 @@ from bokeh.io import output_notebook, reset_output
 from wellcomeml.viz.palettes import Wellcome33, WellcomeBackground
 
 
-def visualize_clusters(reduced_points: list, RADIUS: float,
-                       ALPHA: float, output_in_notebook: bool):
+def visualize_clusters(reduced_points: list, radius: float,
+                       alpha: float, output_in_notebook: bool,
+                       output_file_path: str = 'cluster_viz.html'):
 
 
     """
@@ -12,6 +13,15 @@ def visualize_clusters(reduced_points: list, RADIUS: float,
     Args:
         reduced_points: list
         List of list of reduced points. Available at cluster.reduced_points
+        radius: float
+        Radius of the circles in the scatter plot
+        alpha: float
+        Percentage of fillness when coloring the circle
+        output_in_notebook: bool
+        Boolean variable, it True the plot is in the notebook,
+        if False the plot is in a new html page
+        output_file_path: str = 'cluster_viz.html'
+        By default equal to 'cluster_viz.html'
 
     Returns:
         None prints a bokeh figure to html file in new page
@@ -20,20 +30,20 @@ def visualize_clusters(reduced_points: list, RADIUS: float,
 
     blue_wellcome = str(Wellcome33[0])
     well_background = str(WellcomeBackground)
-    TOOLS = ('hover, pan, wheel_zoom, zoom_in, zoom_out, reset, save')
-    TOOLTIPS = [("index", "$index"), ("(x,y)", "($x, $y)")]
+    tools = ('hover, pan, wheel_zoom, zoom_in, zoom_out, reset, save')
+    tooltips = [("index", "$index"), ("(x,y)", "($x, $y)")]
 
     p = figure(title="Cluster visualisation", toolbar_location="above",
-               tools=TOOLS, tooltips=TOOLTIPS,
+               tools=tools, tooltips=tooltips,
                background_fill_color=well_background)
 
-    p.scatter(reduced_points[0], reduced_points[1], radius=RADIUS,
-              fill_color=blue_wellcome, line_color=None, alpha=ALPHA)
+    p.scatter(reduced_points[0], reduced_points[1], radius=radius,
+              fill_color=blue_wellcome, line_color=None, alpha=alpha)
 
     reset_output()
     if output_in_notebook == True:
         output_notebook()
         show(p)
     else:
-        output_file("cluster_viz.html")
+        output_file(output_file_path)
         show(p)
