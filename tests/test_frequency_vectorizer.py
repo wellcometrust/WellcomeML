d272de193a79aec71525e5be312f8315d55fa3bb
aCampello <a.campello@wellcome.ac.uk>
2021-07-30 11:07:24 +0100

    Add test for transformations

--- tests/test_frequency_vectorizer.py
+++ tests/test_frequency_vectorizer.py
@@ -1,4 +1,6 @@
 # encoding: utf-8
+import pytest
+
 from wellcomeml.ml.frequency_vectorizer import WellcomeTfidf
 
 
@@ -25,3 +27,21 @@ def test_save_and_load(tmpdir):
     X_loaded = vec.load_transformed(str(tmpfile))
 
     assert (X_loaded != X_embed).sum() == 0
+
+
+def test_fit_transform_and_transform():
+    X = [
+        "This is a sentence",
+        "This is another one",
+        "This is a third sentence",
+        "Wellcome is a global charitable foundation",
+        "We want everyone to benefit from science's potential to improve health and save lives."
+    ]
+
+    text_vectorizer = WellcomeTfidf()
+    X_embed = text_vectorizer.fit_transform(X)
+
+    X_embed_2 = text_vectorizer.transform(X)
+
+    # Asserts that the result of transform is almost the same as fit transform
+    assert (X_embed-X_embed_2).sum() == pytest.approx(0, abs=1e-6)
