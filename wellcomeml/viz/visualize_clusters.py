164b853e56a0d6f1f76c83ce761fda3fba579f5f
aCampello <a.campello@wellcome.ac.uk>
2021-08-03 12:25:08 +0100

    update viz function
    Added legend
    
    Fixes #339

--- wellcomeml/viz/visualize_clusters.py
+++ wellcomeml/viz/visualize_clusters.py
@@ -1,33 +1,37 @@
+import numpy as np
 import pandas as pd
-from bokeh.plotting import figure, output_file, show
 from bokeh.io import output_notebook, reset_output
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Legend, LegendItem
+from bokeh.plotting import figure, output_file, show
+
 from wellcomeml.viz.palettes import (Wellcome33,
                                      WellcomeBackground, WellcomeNoData)
 
 
-def visualize_clusters(clustering, radius: float = 0.05,
-                       alpha: float = 0.8, output_in_notebook: bool = True,
+def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.8,
                        plot_width: int = 600, plot_height: int = 600,
-                       output_file_path: str = 'cluster_viz.html'):
+                       output_in_notebook: bool = True,
+                       output_file_path: str = 'cluster_viz.html',
+                       palette: list = Wellcome33):
 
     """
-    Visualises clusters and shows basic information
-    Args:
-        clustering: class
-        radius: float
-        Radius of the circles in the scatter plot
-        alpha: float
-        Percentage of fillness when coloring the circle
-        output_in_notebook: bool
-        Boolean variable, it True the plot is in the notebook,
-        if False the plot is in a new html page
-        output_file_path: str = 'cluster_viz.html'
-        By default equal to 'cluster_viz.html'
+    This function creates a plot of the clusters
 
-    Returns:
-        None prints a bokeh figure to html file in new page
+    Parameters
+    ----------
+        clustering : class
+        radius : float, default: 0.05
+        alpha : float, default: 0.8
+        plot_width : int, default: 600
+        plot_height : int, default: 600
+        output_in_notebook : bool, default: True
+        output_file_path : str, default: 'cluster_viz.html'
+        palette : list, default: Wellcome33
 
+    Returns
+    -------
+        None
+            Prints a bokeh figure
     """
 
     reduced_points = clustering.reduced_points
@@ -37,11 +41,12 @@ def visualize_clusters(clustering, radius: float = 0.05,
     data['cluster_id'] = data['cluster_id'].astype(str)
     data['Keywords'] = clustering.cluster_kws
 
-    Wellcome33_palette = [str(x) for x in Wellcome33]
+    palette = [str(x) for x in palette]
     well_background = str(WellcomeBackground)
     clusters = list(data['cluster_id'])
     clusters = list(map(int, clusters))
-    data['colors'] = [(Wellcome33_palette[x % 33]
+    clusters_uniq = np.unique(clusters)
+    data['colors'] = [(palette[x % len(palette)]
                        if x != -1 else str(WellcomeNoData)) for x in clusters]
     source = ColumnDataSource.from_df(data)
 
@@ -49,13 +54,35 @@ def visualize_clusters(clustering, radius: float = 0.05,
     tooltips = [("index", "$index"), ("(x,y)", "($x, $y)"),
                 ("cluster", "@cluster_id"), ("keywords", "@Keywords")]
 
-    p = figure(title="Cluster visualisation", toolbar_location="above",
+    p = figure(title="Cluster visualization", toolbar_location="above",
                plot_width=plot_width, plot_height=plot_height,
                tools=tools, tooltips=tooltips,
-               background_fill_color=well_background)
+               background_fill_color=well_background,
+               sizing_mode='scale_width')
 
-    p.scatter(x='X', y='Y', radius=radius, source=source,
-              color='colors', line_color=None, alpha=alpha)
+    R = []
+    for x in clusters_uniq:
+        df = data[data['cluster_id'] == str(x)]
+        r = p.circle(x="X", y="Y", color="colors", source=df)
+        R += [r]
+
+    if len(clusters_uniq) > 36:
+        median = len(R) // 2
+        legend1 = Legend(items=[(str(s), [r]) for s, r in
+                                zip(clusters_uniq[:median], R[:median])])
+        legend2 = Legend(items=[(str(s), [r]) for s, r in
+                                zip(clusters_uniq[median:], R[median:])])
+        p.add_layout(legend1, 'right')
+        p.add_layout(legend2, 'right')
+    else:
+        legend = Legend(items=[(str(s), [r]) for s, r in
+                               zip(clusters_uniq, R)])
+        p.add_layout(legend, 'right')
+
+    p.legend.title = "Cluster ID"
+    p.legend.label_text_font_size = "10px"
+    p.legend.background_fill_color = str(WellcomeBackground)
+    p.legend.click_policy = "hide"
 
     reset_output()
     if output_in_notebook:
