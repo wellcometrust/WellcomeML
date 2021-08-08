058e8ce914eb5d88032fbde9827b175daee10ec8
Federica Trevisan <trevisan.federica3@gmail.com>
2021-08-08 19:49:59 +0200

    add filter button
    Generated a new column of the dataframe with randomly generated strings ("Accepted" or "Rejected") Created a dropdown button that works with some javascript code by filtering the plot rsults based on the string value
    
    By achieving this the problem is that the legend is no more present, nor is clickable
    
    Fixes #344
    Reopens #339

--- wellcomeml/viz/visualize_clusters.py
+++ wellcomeml/viz/visualize_clusters.py
@@ -1,10 +1,13 @@
-import numpy as np
+
+import random
 import pandas as pd
 from bokeh.io import output_notebook, reset_output
-from bokeh.models import Legend
+from bokeh.models import Dropdown, ColumnDataSource, CustomJS
 from bokeh.plotting import figure, output_file, show
 from wellcomeml.viz.palettes import (Wellcome33,
                                      WellcomeBackground, WellcomeNoData)
+from bokeh.layouts import column
+from bokeh.events import MenuItemClick
 
 
 def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.5,
@@ -38,18 +41,76 @@ def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.5,
     data['cluster_id'] = clustering.cluster_ids
     data['cluster_id'] = data['cluster_id'].astype(str)
     data['Keywords'] = clustering.cluster_kws
+    # create 'category' column and generate it with random values
+    data['category'] = pd.Series(random.choices(['Accepted', 'Rejected'],
+                                                weights=[5, 1], k=len(data)))
 
     palette = [str(x) for x in palette]
     well_background = str(WellcomeBackground)
     clusters = list(data['cluster_id'])
     clusters = list(map(int, clusters))
     clusters_uniq = np.unique(clusters)
+
     data['colors'] = [(palette[x % len(palette)]
                        if x != -1 else str(WellcomeNoData)) for x in clusters]
 
     tools = ('hover, pan, wheel_zoom, zoom_in, zoom_out, reset, save')
     tooltips = [("index", "$index"), ("(x,y)", "($x, $y)"),
                 ("cluster", "@cluster_id"), ("keywords", "@Keywords")]
+
+    source = ColumnDataSource(data)
+    dropdown_options = [('All', 'All'), None] + [
+        (cat, cat) for i, cat in enumerate(sorted(data['category'].unique()),
+                                           2)]
+    # Generate dropdown widget
+    dropdown = Dropdown(label='Category', button_type='default',
+                        menu=dropdown_options)
+    filtered_data = ColumnDataSource(data)
+
+    # Callback
+    callback = CustomJS(
+        args=dict(unfiltered_data=source, filtered_data=filtered_data),
+        code="""
+
+    var data = unfiltered_data.data;
+    var cat = cb_obj.item;
+
+    function generateNewDataObject(oldDataObject){
+        var newDataObject = {}
+        for (var key of Object.keys(oldDataObject)){
+            newDataObject[key] = [];
+        }
+        return newDataObject
+
+    }
+
+    function addRowToAccumulator(accumulator, dataObject, index) {
+        for (var key of Object.keys(dataObject)){
+            accumulator[key][index] = dataObject[key][index];
+        }
+        return accumulator;
+    }
+
+    if (cat === 'All'){
+        data = unfiltered_data.data;
+    } else {
+        var new_data =  generateNewDataObject(data);
+        for (var i = 0; i <= unfiltered_data.data['category'].length; i++){
+            if (unfiltered_data.data['category'][i] == cat) {
+                new_data = addRowToAccumulator(new_data, unfiltered_data.data,
+                                               i);
+            }
+        }
+        data = new_data;
+    }
+
+    filtered_data.data = data;
+    filtered_data.change.emit();
+    """
+    )
+
+    # Link actions
+    dropdown.js_on_event(MenuItemClick, callback)
 
     p = figure(title="Cluster visualization", toolbar_location="above",
                plot_width=plot_width, plot_height=plot_height,
@@ -85,7 +146,7 @@ def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.5,
     reset_output()
     if output_in_notebook:
         output_notebook()
-        show(p)
+        show(column(dropdown, p))
     else:
         output_file(output_file_path)
-        show(p)
+        show(column(dropdown, p))
