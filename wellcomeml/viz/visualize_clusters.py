54aaf692aea3c191859ef63639e2325b982e6aba
Federica Trevisan <trevisan.federica3@gmail.com>
2021-08-11 13:03:01 +0200

    filter button and 2 cols legend
    Updated viz function in order to add interactivity to the plot. It has a filter button and a working legend.
    
    Fixes #344 #339

--- wellcomeml/viz/visualize_clusters.py
+++ wellcomeml/viz/visualize_clusters.py
@@ -1,7 +1,7 @@
-import random
+import numpy as np
 import pandas as pd
 from bokeh.io import output_notebook, reset_output
-from bokeh.models import Dropdown, ColumnDataSource, CustomJS
+from bokeh.models import Legend, Dropdown, ColumnDataSource, CustomJS
 from bokeh.plotting import figure, output_file, show
 from wellcomeml.viz.palettes import (Wellcome33,
                                      WellcomeBackground, WellcomeNoData)
@@ -9,7 +9,8 @@ from bokeh.layouts import column
 from bokeh.events import MenuItemClick
 
 
-def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.5,
+def visualize_clusters(clustering, filter_list, radius: float = 0.05,
+                       alpha: float = 0.5,
                        plot_width: int = 600, plot_height: int = 600,
                        output_in_notebook: bool = True,
                        output_file_path: str = 'cluster_viz.html',
@@ -20,6 +21,7 @@ def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.5,
     Parameters
     ----------
         clustering : class
+        filter_list : list
         radius : float, default: 0.05
         alpha : float, default: 0.5
         plot_width : int, default: 600
@@ -40,14 +42,13 @@ def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.5,
     data['cluster_id'] = clustering.cluster_ids
     data['cluster_id'] = data['cluster_id'].astype(str)
     data['Keywords'] = clustering.cluster_kws
-    # create 'category' column and generate it with random values
-    data['category'] = pd.Series(random.choices(['Accepted', 'Rejected'],
-                                                weights=[5, 1], k=len(data)))
+    data['category'] = filter_list
 
     palette = [str(x) for x in palette]
     well_background = str(WellcomeBackground)
     clusters = list(data['cluster_id'])
     clusters = list(map(int, clusters))
+    clusters_uniq = np.unique(clusters)
     data['colors'] = [(palette[x % len(palette)]
                        if x != -1 else str(WellcomeNoData)) for x in clusters]
 
@@ -55,21 +56,39 @@ def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.5,
     tooltips = [("index", "$index"), ("(x,y)", "($x, $y)"),
                 ("cluster", "@cluster_id"), ("keywords", "@Keywords")]
 
-    source = ColumnDataSource(data)
     dropdown_options = [('All', 'All'), None] + [
         (cat, cat) for i, cat in enumerate(sorted(data['category'].unique()),
                                            2)]
     # Generate dropdown widget
     dropdown = Dropdown(label='Category', button_type='default',
                         menu=dropdown_options)
-    filtered_data = ColumnDataSource(data)
+
+    p = figure(title="Cluster visualization", toolbar_location="above",
+               plot_width=plot_width, plot_height=plot_height,
+               tools=tools, tooltips=tooltips,
+               background_fill_color=well_background,
+               sizing_mode='scale_width')
+
+    R = []
+    sources = []
+    filtered_sources = []
+    for x in clusters_uniq:
+        data_cluster_id_unfiltered = data[data['cluster_id'] == str(x)]
+
+        sources.append(ColumnDataSource(data_cluster_id_unfiltered))
+        filtered_sources.append(ColumnDataSource(data_cluster_id_unfiltered))
+
+        r = p.circle(x="X", y="Y", radius=radius, fill_alpha=alpha,
+                     color="colors", source=filtered_sources[-1])
+
+        R += [r]
 
     # Callback
     callback = CustomJS(
-        args=dict(unfiltered_data=source, filtered_data=filtered_data),
+        args=dict(sources=sources, filtered_sources=filtered_sources),
         code="""
 
-    var data = unfiltered_data.data;
+    var data = []
     var cat = cb_obj.item;
 
     function generateNewDataObject(oldDataObject){
@@ -78,7 +97,6 @@ def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.5,
             newDataObject[key] = [];
         }
         return newDataObject
-
     }
 
     function addRowToAccumulator(accumulator, dataObject, index) {
@@ -88,35 +106,47 @@ def visualize_clusters(clustering, radius: float = 0.05, alpha: float = 0.5,
         return accumulator;
     }
 
-    if (cat === 'All'){
-        data = unfiltered_data.data;
-    } else {
-        var new_data =  generateNewDataObject(data);
-        for (var i = 0; i <= unfiltered_data.data['category'].length; i++){
-            if (unfiltered_data.data['category'][i] == cat) {
-                new_data = addRowToAccumulator(new_data, unfiltered_data.data,
-                                               i);
-            }
+    if (cat === 'All') {
+        for (var i = 0; i < sources.length; i++) {
+            data.push(sources[i].data);
         }
-        data = new_data;
+    } else {
+        for (var i = 0; i < sources.length; i++) {
+            let new_data =  generateNewDataObject(sources[i].data);
+            for (var j = 0; j <= sources[i].data['category'].length; j++) {
+                if (sources[i].data['category'][j] == cat) {
+                    new_data = addRowToAccumulator(new_data, sources[i].data,
+                                                   j);
+                }
+            }
+            data[i] = new_data
+        }
     }
-
-    filtered_data.data = data;
-    filtered_data.change.emit();
+     for (var i = 0; i < sources.length; i++) {
+        filtered_sources[i].data = data[i]
+        filtered_sources[i].change.emit()
+    }
     """
     )
-
-    # Link actions
     dropdown.js_on_event(MenuItemClick, callback)
 
-    p = figure(title="Cluster visualization", toolbar_location="above",
-               plot_width=plot_width, plot_height=plot_height,
-               tools=tools, tooltips=tooltips,
-               background_fill_color=well_background,
-               sizing_mode='scale_width')
+    if len(clusters_uniq) > 36:
+        median = len(R) // 2
+        legend1 = Legend(items=[(str(s), [r]) for s, r in
+                                zip(clusters_uniq[:median], R[:median])])
+        legend2 = Legend(items=[(str(s), [r]) for s, r in
+                                zip(clusters_uniq[median:], R[median:])])
+        p.add_layout(legend1, 'right')
+        p.add_layout(legend2, 'right')
+    else:
+        legend = Legend(items=[(str(s), [r]) for s, r in
+                               zip(clusters_uniq, R)])
+        p.add_layout(legend, 'right')
 
-    p.circle(x='X', y='Y', source=filtered_data, radius=radius,
-             fill_alpha=alpha, color="colors")
+    p.legend.title = "Cluster ID"
+    p.legend.label_text_font_size = "10px"
+    p.legend.background_fill_color = str(WellcomeBackground)
+    p.legend.click_policy = "hide"
 
     reset_output()
     if output_in_notebook:
